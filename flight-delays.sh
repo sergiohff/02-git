32df13cd843c7c09cd8deef65d3b60b9f200c9a8
Sergio Lemos Hermsdorff <sergiolemoshff@gmail.com>
2020-08-17 10:03:30 -0300

    Adicionado o Feature #2 listado nas issues

--- flight-delays.sh
+++ flight-delays.sh
@@ -2,7 +2,7 @@
 
 source functions.sh
 
-while getopts ":dt" opt; do
+while getopts ":dtn" opt; do
     case ${opt} in
         d ) # process option h
             shift # Removes de First Argument from the queue
@@ -11,9 +11,25 @@ while getopts ":dt" opt; do
 	    test -f /2007.tar && echo "$FILE já descompactado."
 	    bzip2 -d 2006.tar.bz2
             bzip2 -d 2007.tar.bz2
+	    mv 2006.tar 2006
+	    mv 2007.tar 2007
         ;;
         t ) # process option t
         ;;
+	n ) # Listar atrasos por ano
+	     echo "$2"
+	     contador_atrasos=0
+	     while IFS=, read -ra arr; do
+		if [[ ${arr[0]} -eq 2006 ]]; then
+			if [[ ${arr[14]} -gt 0 ]]; then
+				contador_atrasos=$(($contador_atrasos + 1))
+			fi
+		fi
+	     done < $2
+	     echo "O número de atrasos neste ano foram:"
+	     echo "$contador_atrasos"
+
+	;;
         \? ) echo "Usage: flight-delays.sh [-d] [-t]"
         ;;
   esac
